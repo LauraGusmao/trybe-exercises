8c973c7f2444833bba8393c42d0deba615559f2a
LauraGusmao <laurargus@gmail.com>
2021-03-24 20:16:07 -0300

    Exercícios 1-5

---
+++ bloco_4/dia_1/exercicio.js
@@ -0,0 +1,59 @@
+// Exercício 1
+let a = 3;
+let b = 7;
+
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+
+// Exercício 2
+let num1 = 4;
+let num2 = 9;
+
+if (num1 > num2) {
+  console.log (num1);
+} else {
+  console.log (num2);
+}
+
+// Exercício 3
+let num3 = 11;
+
+if (num1 > num2 && num1 > num3) {
+  console.log (num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log (num2);
+} else {
+  console.log (num3);
+}
+
+// Execício 4
+let num4 = -8;
+
+if (num4 > 0) {
+  console.log ('positive');
+} else if (num4 < 0) {
+  console.log ('negative');
+} else {
+  console.log ('zero');
+}
+
+// Exercício 5
+let ang1 = 90;
+let ang2 = 110;
+let ang3 = 20;
+
+let sumAng = ang1 + ang2 + ang3
+
+if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+  console.log('error');
+} else if (sumAng === 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Evercício 6
+
