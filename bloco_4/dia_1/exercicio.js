be860816802264bcd753ef0ab6c7bc6c537bb4f7
LauraGusmao <laurargus@gmail.com>
2021-03-25 18:37:21 -0300

    Exercícios 4.1 finalizados

--- bloco_4/dia_1/exercicio.js
+++ bloco_4/dia_1/exercicio.js
@@ -101,3 +101,63 @@ if (grade < 0 || grade > 100) {
 }
 
 // Exercício 8
+let num5 = 6;
+let num6 = 3;
+let num7 = 2;
+
+if (num5 % 2 === 0 || num6 % 2 === 0 || num7 % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Exercício 9
+if (num5 % 2 !== 0 || num6 % 2 !== 0 || num7 % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Exercício 10
+let costPrice = 20;
+let salePrice = 35;
+
+if (costPrice >= 0 && salePrice >= 0) {
+  let profit = (salePrice - (costPrice + costPrice*0.2))*1000;
+  console.log(profit);
+} else {
+  console.log('Error');
+}
+
+// Exercício 11
+
+let aliquotINSS;
+let aliquotIR;
+
+let grossSalary = 2200.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
